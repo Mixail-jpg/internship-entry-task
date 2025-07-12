467a009aac9d3f4876914311c99d8128ab272f25
Mikhail <mixail.chernyshev.00@gmail.com>
2025-07-12 16:45:14 +0500

    Initial commit

---
+++ ticTacToeRestApi/Controllers/GameController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ticTacToeRestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GameController : ControllerBase
+    {
+        
+    }
+}
